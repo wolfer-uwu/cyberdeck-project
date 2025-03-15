fd2da6f7bfb128fb79ca0fb0aa82ca7afbf71f7b
Wolfer <z.crysis.time2021@gmail.com>
2025-03-15 18:12:43 +0800

    Update keybd.ino
    Changed keyboard firmware

--- keyboard/keybd.ino
+++ keyboard/keybd.ino
@@ -1,33 +1,43 @@
-#include <EasyHID.h>            //пасяба Алексу UwU "https://github.com/GyverLibs/EasyHID"
 #include <Keypad.h>
-const byte Row = 5;
-const byte Col = 10;      //максимум 60 клавишь!!!!!
-const char k5x10 [Row][Col] = {
-  {'KEY_1', 'KEY_2', 'KEY_3', 'KEY_4', 'KEY_5', 'KEY_6', 'KEY_7', 'KEY_8', 'KEY_9', 'KEY_0'},
-  {'KEY_Q', 'KEY_W', 'KEY_E', 'KEY_R', 'KEY_T', 'KEY_Y', 'KEY_U', 'KEY_I', 'KEY_O', 'KEY_P'},
-  {'KEY_A', 'KEY_S', 'KEY_D', 'KEY_F', 'KEY_G', 'KEY_H', 'KEY_J', 'KEY_K', 'KEY_L', 'KEY_BACKSLASH'},
-  {'KEY_Z', 'KEY_X', 'KEY_C', 'KEY_V', 'KEY_B', 'KEY_N', 'KEY_M', 'KEY_COMMA', 'KEY_PERIOD', 'KEY_SLASH'},
-  {'KEY_ENTER', 'KEY_ESC', 'KEY_ALT', 'KEY_TAB', 'kEY_SPACE', 'KEY_SPACE', 'KEY_ARROW_LEFT', 'KEY_ARROW_UP', 'KEY_ARROW_DOWN', 'KEY_ARROW_RIGHT'}
+#include <Keyboard.h>
+
+const byte ROWS = 5; // 5 рядов
+const byte COLS = 10; // 10 столбцов
+
+// массив раскладки
+char keys[ROWS][COLS] = {
+  {'1', '2', '3', '4', '5', '6', '7', '8', '9', '0'},
+  {'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p'},
+  {'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', '\'},
+  {'z', 'x', 'c', 'v', 'b', 'n', 'm', ',', '.', '/'},
+  {'\n', 27, 18, 15, ' ', ' ', KEY_LEFT_ARROW, KEY_UP_ARROW, KEY_DOWN_ARROW, KEY_RIGHT_ARROW}
 };
-byte rowPin[Row] = {3, 5, 6, 7, 8};                  //укажите любые пины, кроме D2 и D4 для строк
-byte colPin[Col] = {9, 10, 11, 12, 13, 14, 15, 16, 17, 18};        //укажите любые пины, кроме D2 и D4 для столбцов
-Keypad keypad = Keypad(makeKeymap(k5x10), rowPin, colPin, Row, Col);        //создается объект keypad, для упрощения работы с матричной клавиатурой
+
+byte rowPins[ROWS] = {2, 3, 4, 5, 6}; // Ряды
+byte colPins[COLS] = {7, 8, 9, 10, 11, 12, 13, A0, A1, A2}; // Столбцы
+
+Keypad keypad = Keypad(makeKeymap(keys), rowPins, colPins, ROWS, COLS);
+
 void setup() {
-  HID.begin();        //инит HID шины
+  Keyboard.begin(); // Начинаем использование библиотеки Keyboard
 }
 
 void loop() {
-    static uint32_t timer = millis();      //DELAY не использовать!!! только millis!!!!!
-  if (millis() - timer >= 1000) {
-    timer = millis();
-  }
-  char key = keypad.getKey();
-  if (key) {
-    Keyboard.press(key);
-    Keyboard.release(key);
-  }
-  HID.tick();
-}
+  char key = keypad.getKey(); // Получаем нажатую клавишу
 
-// код будет работать с любой простейшей матричной клавиатурой. в массиве можете указать нужные вам клавиши, а ниже, пины.
-// совыетую подключать столбцы через диоды, воизбежания дребезга кнопок, но и без них работает.
+  if (key) { // Если клавиша нажата
+    if (key == '\n') {
+      Keyboard.press(KEY_RETURN); // Нажимаем ENTER
+      Keyboard.release(KEY_RETURN);
+    } else if (key == 27) { // ESC
+      Keyboard.press(KEY_ESCAPE);
+      Keyboard.release(KEY_ESCAPE);
+    } else if (key == ' ') {
+      Keyboard.press(KEY_SPACE);
+      Keyboard.release(KEY_SPACE);
+    } else {
+      Keyboard.press(key); // Нажимаем кнопку
+      Keyboard.release(key); // Отпускаем кнопку
+    }
+  }
+}
