b528728025c56165d9a69eb671515dcf891a1956
Wolfer <z.crysis.time2021@gmail.com>
2025-03-15 22:39:33 +0800

    Update keybd.ino
    keybd bug fixed

--- keyboard/keybd.ino
+++ keyboard/keybd.ino
@@ -8,13 +8,13 @@ const byte COLS = 10; // 10 столбцов
 char keys[ROWS][COLS] = {
   {'1', '2', '3', '4', '5', '6', '7', '8', '9', '0'},
   {'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p'},
-  {'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', '\'},
+  {'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', '?'},
   {'z', 'x', 'c', 'v', 'b', 'n', 'm', ',', '.', '/'},
-  {'\n', 27, 18, 15, ' ', ' ', KEY_LEFT_ARROW, KEY_UP_ARROW, KEY_DOWN_ARROW, KEY_RIGHT_ARROW}
+  {'\n', 27, 'KEY_LEFT_CTRL', KEY_CAPS_LOCK, ' ', ' ', KEY_CAPS_LOCK, KEY_BACKSPACE, KEY_LEFT_ALT, KEY_TAB}
 };
 
 byte rowPins[ROWS] = {2, 3, 4, 5, 6}; // Ряды
-byte colPins[COLS] = {7, 8, 9, 10, 11, 16, 14, 15, 18, 19}; // Столбцы
+byte colPins[COLS] = {7, 8, 9, 10, 16, 14, 15, 18, 19, 20}; // Столбцы
 
 Keypad keypad = Keypad(makeKeymap(keys), rowPins, colPins, ROWS, COLS);
 
@@ -30,11 +30,10 @@ void loop() {
       Keyboard.press(KEY_RETURN); // Нажимаем ENTER
       Keyboard.release(KEY_RETURN);
     } else if (key == 27) { // ESC
-      Keyboard.press(KEY_ESCAPE);
-      Keyboard.release(KEY_ESCAPE);
-    } else if (key == ' ') {
-      Keyboard.press(KEY_SPACE);
-      Keyboard.release(KEY_SPACE);
+      Keyboard.press(KEY_ESC);
+      Keyboard.release(KEY_ESC);
+    } else if (key == 'KEY_LEFT_CTRL') {
+      Keyboard.press(KEY_LEFT_CTRL);
     } else {
       Keyboard.press(key); // Нажимаем кнопку
       Keyboard.release(key); // Отпускаем кнопку
